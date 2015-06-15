a26074cac4e6032ee25ffcf16586ecd1d151201a
Carl Banbury <carl@googolmail.co.uk>
2015-06-15 11:41:48 +0100

    optional parameter for where to store database
    update to README
    
    adding test for loading config with custom path

--- test/config.js
+++ test/config.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var assert = require('assert');
+var rewire = require('rewire');
 
-var config = require('../lib/config');
+var config = rewire('../lib/config');
 
 describe('config', function () {
     it('has dbDir', function () {
         assert.ok(config.dbDir);
+    });
+
+    it('has custom dbDir', function() {
+        var expected = '/foo/bar';
+        process.env.MAXMIND_DB_DIR = expected;
+
+        config = rewire('../lib/config');
+
+        assert.equal(config.dbDir, expected);
+        delete process.env.MAXMIND_DB_DIR;
     });
 
     it('has geoIpDbs', function () {
@@ -29,4 +40,3 @@ describe('config', function () {
         assert.ok(config.urlPath);
     });
 });
-
