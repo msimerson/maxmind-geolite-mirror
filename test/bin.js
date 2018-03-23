c374c74a2033e90e43e69c634c75a7e5b86b2ae9
Tom Lee <tlee@mapbox.com>
2018-03-23 13:20:31 -0400

    tmp is a devDependency, let's own it

--- test/bin.js
+++ test/bin.js
@@ -4,22 +4,11 @@ const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const assert = require('assert');
-
-function makeTempDir() {
-    try {
-        // put artifacts in temp directory when possible
-        return require('tmp').dirSync({ unsafeCleanup: true });
-    }
-    catch (err) {
-        return { name: '/tmp', removeCallback: () => {} };
-    }
-}
+const tmp = require('tmp');
 
 describe('maxmind-geolite-mirror', () => {
-    const timestamp = +new Date();
-
     it('can be required', (done) => {
-        const tmpDir = makeTempDir();
+        const tmpDir = tmp.dirSync({ unsafeCleanup: true });
         process.env.MAXMIND_DB_DIR = tmpDir.name;
         const fetcher = require('../bin/maxmind-geolite-mirror');
         assert.ok(fetcher);
@@ -41,9 +30,9 @@ describe('maxmind-geolite-mirror', () => {
     });
 
     it('can handle gzip', (done) => {
-        const tmpDir = makeTempDir();
+        const tmpDir = tmp.dirSync({ unsafeCleanup: true });
         const fetcher = require('../bin/maxmind-geolite-mirror').download;
-        const outfile = path.join(tmpDir.name, `sample-${timestamp}-1.mmdb`);
+        const outfile = path.join(tmpDir.name, `sample-1.mmdb`);
         fetcher(outfile, { path: path.join(__dirname, 'fixtures', 'sample.mmdb.gz')}, (err) => {
             assert.equal(fs.readFileSync(outfile).toString().trim(), 'success');
             tmpDir.removeCallback();
@@ -52,9 +41,9 @@ describe('maxmind-geolite-mirror', () => {
     });
 
     it('can handle tarball', (done) => {
-        const tmpDir = makeTempDir();
+        const tmpDir = tmp.dirSync({ unsafeCleanup: true });
         const fetcher = require('../bin/maxmind-geolite-mirror').download;
-        const outfile = path.join(tmpDir.name, `sample-${timestamp}-2.mmdb`);
+        const outfile = path.join(tmpDir.name, `sample-2.mmdb`);
         fetcher(outfile, { path: path.join(__dirname, 'fixtures', 'sample.tar.gz')}, (err) => {
             assert.equal(fs.readFileSync(outfile).toString().trim(), 'success');
             tmpDir.removeCallback();
@@ -63,9 +52,9 @@ describe('maxmind-geolite-mirror', () => {
     });
 
     it('can handle multi-mmdb tarball', (done) => {
-        const tmpDir = makeTempDir();
+        const tmpDir = tmp.dirSync({ unsafeCleanup: true });
         const fetcher = require('../bin/maxmind-geolite-mirror').download;
-        const outfile = path.join(tmpDir.name, `sample-${timestamp}-3.mmdb`);
+        const outfile = path.join(tmpDir.name, `sample-3.mmdb`);
         fetcher(outfile, { path: path.join(__dirname, 'fixtures', 'sample-multi.tar.gz')}, (err) => {
             assert.equal(fs.readFileSync(outfile).toString().trim(), 'success');
             tmpDir.removeCallback();
