3b465b6db9ff4221172ba5179a2fec7e03f027fa
Tom Lee <tlee@mapbox.com>
2018-03-23 14:44:30 -0400

    restore basic bin.js test

---
+++ test/bin.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('maxmind-geolite-mirror', () => {
+    it('can be required', () => {
+        const fetcher = require('../bin/maxmind-geolite-mirror');
+        assert.ok(fetcher);
+    })
+});
